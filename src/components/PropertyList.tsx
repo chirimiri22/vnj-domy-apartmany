29293172c8be809eb35da1c966d6e89dd992b996
Miriam Zednickova <mitcamit@gmail.com>
2025-08-09 11:43:54 +0200

    make property list clickable

--- src/components/PropertyList.tsx
+++ src/components/PropertyList.tsx
@@ -1,10 +1,12 @@
 import { Container, Stack, Typography } from "@mui/material";
-import { DataGridPremium, type GridColDef } from "@mui/x-data-grid-premium";
+import { DataGridPremium, type GridColDef, type GridRowSelectionModel, useGridApiRef } from "@mui/x-data-grid-premium";
 import type { Apartment } from "../model/Apartment.ts";
+import { useEffect, useState } from "react";
 
 type Props = {
   apartments: ApartmentCol[];
-  onClickProperty?: (id: string) => void;
+  onClickProperty?: (houseId: string, apartmentId: string) => void;
+  selectedApartmentId?: string;
 };
 
 type ApartmentCol = Apartment & { houseId?: string; houseName?: string };
@@ -13,9 +15,9 @@ export const columns: GridColDef<ApartmentCol>[] = [
   { field: "houseName", headerName: "Název domu", width: 120 },
   { field: "number", headerName: "Číslo bytu", width: 85 },
   { field: "layout", headerName: "Dispozice", width: 85 },
-  { field: "usable_area", headerName: "Užitná plocha (m²)", width: 150 },
-  { field: "totalArea", headerName: "Celková plocha (m²)", width: 150 },
-  { field: "balcony", headerName: "Balkón (m²)", width: 100 },
+  { field: "usable_area", headerName: "Užitná plocha (m²)", width: 120 },
+  { field: "totalArea", headerName: "Celková plocha (m²)", width: 120 },
+  { field: "balcony", headerName: "Balkón (m²)", width: 85 },
   { field: "garden", headerName: "Zahrada (m²)", width: 110 },
   { field: "storage", headerName: "Sklep (m²)", width: 95 },
   { field: "garage", headerName: "Garáž", width: 75 },
@@ -23,14 +25,22 @@ export const columns: GridColDef<ApartmentCol>[] = [
   { field: "status", headerName: "Stav", width: 120 },
 ];
 
-export const PropertyList = ({ apartments, onClickProperty }: Props) => {
+export const PropertyList = ({ apartments, onClickProperty, selectedApartmentId }: Props) => {
+  const apiRef = useGridApiRef();
+
+  useEffect(() => {
+    if (selectedApartmentId) {
+      apiRef?.current?.selectRow(selectedApartmentId, true); // highlight row
+    }
+  }, [selectedApartmentId, apartments, apiRef]);
   return (
     <Stack pb={6}>
       <DataGridPremium
+        apiRef={apiRef}
         rows={apartments}
         columns={columns}
         onRowClick={(params) => {
-          onClickProperty?.(params.row.houseId);
+          onClickProperty?.(params.row.houseId, params.row.id);
         }}
         hideFooter
       />
--- src/components/property-detail/PropertyDetail.tsx
+++ src/components/property-detail/PropertyDetail.tsx
@@ -14,31 +14,57 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { base } from "../../constants/constants.ts";
 import { apartmentToCol } from "../../utils/apartmentToCol.ts";
 import { useScrollToTopOnLoad } from "../../hook/useScrollToTopOnLoad.ts";
+import type { Apartment } from "../../model/Apartment.ts";
 
 export const PropertyDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const property = data.find((property) => property.id === id);
-
   useScrollToTopOnLoad([id]);
-  if (!id || !property) {
+
+  const house = data.find((property) => property.id === id);
+
+  const [apartment, setApartment] = useState<Apartment>();
+
+  useEffect(() => {
+    if (house) {
+      setApartment(house.apartments[0]);
+    }
+  }, [house]);
+
+  const handleApartmentClick = (_: string, apartmentId: string) => {
+    const selectedApartment = house?.apartments.find((ap) => ap.id === apartmentId);
+    // console.log("Selected apartment:", apartmentId, house?.apartments.map(x => x.id));
+    if (selectedApartment) {
+      setApartment(selectedApartment);
+    }
+  };
+
+  if (!id || !house) {
     return "Property not found";
   }
   return (
     <Stack>
-      <TitleSection house={property} image={visualisation} />
+      <TitleSection house={house} image={visualisation} />
       <Container>
-        <PropertyList apartments={property.apartments.map((ap) => apartmentToCol(ap, property))} />
-        <Stats stats={data[0].apartments[0]} />
-        <Typography mt={5} mb={8}>
-          Dům Alžběta se nachází na pozici 12×3 a nabízí klidné bydlení v moderním rodinném domě s jediným prostorným
-          bytem. Tento dům je ideální pro ty, kteří hledají soukromí, komfort a přímý kontakt s přírodou. Dům vyniká
-          promyšlenou architekturou a vysokým standardem vybavení. Byt RD1B1 má dispozici 3+kk a nabízí užitnou plochu
-          97,74 m², přičemž celková plocha včetně příslušenství činí 103,67 m². K bytu náleží prostorná zahrada o
-          velikosti 171,33 m², balkon o ploše 9 m², sklep o velikosti 9,35 m² a jedno garážové stání. Tento byt byl již
-          prodán.
-        </Typography>
+        <PropertyList
+          apartments={house.apartments.map((ap) => apartmentToCol(ap, house))}
+          onClickProperty={handleApartmentClick}
+          selectedApartmentId={apartment?.id}
+        />
+        {apartment && (
+          <>
+            <Stats stats={apartment} />
+            <Typography mt={5} mb={8}>
+              Dům Alžběta se nachází na pozici 12×3 a nabízí klidné bydlení v moderním rodinném domě s jediným
+              prostorným bytem. Tento dům je ideální pro ty, kteří hledají soukromí, komfort a přímý kontakt s přírodou.
+              Dům vyniká promyšlenou architekturou a vysokým standardem vybavení. Byt RD1B1 má dispozici 3+kk a nabízí
+              užitnou plochu 97,74 m², přičemž celková plocha včetně příslušenství činí 103,67 m². K bytu náleží
+              prostorná zahrada o velikosti 171,33 m², balkon o ploše 9 m², sklep o velikosti 9,35 m² a jedno garážové
+              stání. Tento byt byl již prodán.
+            </Typography>
+          </>
+        )}
       </Container>
 
       <Gallery />
