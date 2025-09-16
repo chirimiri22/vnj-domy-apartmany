87949d92645f56b3bdc7b827e4ac23e2bfcebfed
Miriam Zednickova <mitcamit@gmail.com>
2025-09-16 18:30:22 +0200

    lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeOptions } from "./theme/themeOptions.ts";
 import { LicenseInfo } from "@mui/x-license";
 import { PropertyDetail } from "./components/property-detail/PropertyDetail.tsx";
-import { Header } from "./components/Header.tsx";
 import { Home } from "./components/Home.tsx";
 import { base } from "./constants/constants.ts";
 import { LanguageProvider } from "./contexts/LanguageContext.tsx";
